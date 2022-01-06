217c8b582b7ba2e2d11ce4b7d98bddd4fd00bed2
Zachary Zhao <zhaozeyu@seas.upenn.edu>
2022-01-06 00:41:09 -0500

    first commit

---
+++ internal/log/store.go
@@ -0,0 +1,91 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File // TODO: study the usage here
+	mu       sync.Mutex
+	buf      *bufio.Writer
+	size     uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+// Append persists teh given bytes to the store.
+// Write the length of the record so when reading, know how many bytes to read.
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil { // writes the length of the record
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p) // write to the buffered writer instead of directly to file to reduce the number of system calls
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil { // first flushes writer buffer
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil { // read how many bytes we have to read
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil { // read the whole record
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+// TODO: implement Close()
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
